43469139ecc40094e3584371f0fe1ed47ceb7074
Luuk <lderuiter@storecove.com>
2025-07-15 13:08:14 +0200

    Lets hope

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def serve_data():
 if __name__ == "__main__":
     # Get port from environment variable or use 8080 for local development
     port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True) 
